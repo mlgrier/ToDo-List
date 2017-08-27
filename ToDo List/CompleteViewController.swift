48393437f4b00eb97946e6c2970336dfc618c1ad
Unknown <mlgrier@gmail.com>
2017-08-27 11:45:26 -0500

    added complete view controller

---
+++ ToDo List/CompleteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CompleteViewController.swift
+//  ToDo List
+//
+//  Created by Marco Grier on 8/27/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class CompleteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
