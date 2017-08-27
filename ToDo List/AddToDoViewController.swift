54084f8d0651b45ddc9febcc884a5a39a711f6d8
Unknown <mlgrier@gmail.com>
2017-08-27 10:24:41 -0500

    fix the layout and connected the outlets and actions of the add view

---
+++ ToDo List/AddToDoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AddToDoViewController.swift
+//  ToDo List
+//
+//  Created by Marco Grier on 8/27/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
