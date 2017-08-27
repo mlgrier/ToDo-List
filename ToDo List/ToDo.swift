4a4d9da36f8dfefbf80944acadf9e746a6fe2412
Unknown <mlgrier@gmail.com>
2017-08-27 09:45:12 -0500

    made ToDo class

---
+++ ToDo List/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDo List
+//
+//  Created by Marco Grier on 8/27/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
+
